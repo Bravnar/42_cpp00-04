a04a587de2eb5650b4a3246cbda2aacee54b0029
Stanislav Muravyev <smuravye@c3r3s2.42lausanne.ch>
2024-07-22 13:47:44 +0200

    finished sed_is_for_losers

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smuravye <smuravye@student.42.fr>          +#+  +:+       +#+        */
+/*   By: smuravye <smuravye@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 15:26:52 by smuravye          #+#    #+#             */
-/*   Updated: 2024/07/18 16:32:03 by smuravye         ###   ########.fr       */
+/*   Updated: 2024/07/22 13:42:35 by smuravye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,54 @@
 #include <string>
 #include <fstream>
 
-int	main(int ac, char **av) {
+int	my_replace(std::string &filePath, std::string &what, std::string &by) {
 	
+	/* Open files and check */
+	
+	std::ifstream	inFile(filePath.c_str());
+	if (!inFile) {
+		std::cerr << "Failed to open file " << filePath << std::endl;
+		return 1;
+	}
+	std::string		newName = std::string(filePath) + ".replace";
+	std::ofstream	outFile(newName.c_str());
+	if (!outFile) {
+		std::cerr << "Failed to create file " << newName << std::endl;
+		return 1;
+	}
+
+	/* Loop line by line and replace */
+	
+	std::string	line;
+	
+	while(std::getline(inFile, line)) {
+		std::size_t	pos = 0;
+		while ((pos = line.find(what, pos)) != std::string::npos) {
+			line = line.substr(0, pos) + by + line.substr(pos + what.length());
+			pos += by.length();
+		}
+		outFile << line << std::endl;
+	}
+
+	/* Close the file streams */
+
+	inFile.close();
+	outFile.close();
+
+	return 0;
+}
+
+int	main(int ac, char **av) {
+
 	if (ac != 4) {
 		std::cout << "Wrong number of args\n";
 		return 1;
 	}
 	
-	std::ifstream	inFile(av[1]);
-	std::ofstream	outFile(av[1]);
-	std::string		myString;
-	if (!inFile || !outFile) {
-		std::cerr << "Error opening file with path " << av[1] << std::endl;
-	}
+	std::string		filePath = av[1];
+	std::string		what = av[2];
+	std::string		by = av[3];
+
+	if (my_replace(filePath, what, by)) { return 1; }
 	return 0;
 }
